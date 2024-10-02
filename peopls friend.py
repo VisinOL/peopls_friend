353dee078efe98c06d601c2058dbfcbaf5e8db8f
VisinOL <105442739+VisinOL@users.noreply.github.com>
2024-10-02 18:06:38 +0300

    Update peopls friend.py

--- peopls friend.py
+++ peopls friend.py
@@ -2,28 +2,25 @@ import requests
 import os
 import shutil
 
-def download_picture(folder,link):
-        basename, extension = os.path.splitext(link)
-        if extension not in [".webm", ".mp4"]:
-            response = requests.get(link)
-            response.raise_for_status()
-            filename = f"dog_{item}{extension}"
-            with open(f"{folder}/{filename}", 'wb') as file:
-                file.write(response.content)
-
-def get_url():
+def download_picture(folder):
     url = "https://random.dog/woof.json"
-    response = requests.get(url)
+    params = {"filter": "mp4,webm"}
+    link = requests.get(url, params=params).json()["url"]
+    basename, extension = os.path.splitext(link)
+    response = requests.get(link)
     response.raise_for_status()
-    link = response.json()["url"]
-    return link
+    filename = f"dog_{item}{extension}"
+    with open(f"{folder}/{filename}", 'wb') as file:
+        file.write(response.content)
+    return response.content
 
 if __name__ == '__main__':
     folder = "dogs"
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    elif os.path.exists(folder):
+    if os.path.isdir(folder):
         shutil.rmtree(folder)
+    elif not os.path.isdir(folder):
+        os.makedirs(folder)
+
     count = 50
     for item in range(count):
-        download_picture(folder,get_url())
+        download_picture(folder)
